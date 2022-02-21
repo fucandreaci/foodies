c6554d68d76c5c2ef2ca7fa9ac66208d08cf2c6a
trepode <gabrieladam572@yahoo.it>
2022-02-21 12:47:16 +0100

    Aggiunta creazione di un post e visualizzazione di tutti i post

--- src/main/java/com/azienda/foodies/service/ServiceManager.java
+++ src/main/java/com/azienda/foodies/service/ServiceManager.java
@@ -1,5 +1,6 @@
 package com.azienda.foodies.service;
 
+import com.azienda.foodies.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,8 @@ import com.azienda.foodies.repository.CategoriaRepository;
 import com.azienda.foodies.repository.HashtagRepository;
 import com.azienda.foodies.repository.PostRepository;
 import com.azienda.foodies.repository.UtenteRepository;
+
+import java.util.List;
 
 @Service("ServiceManager")
 @Transactional
@@ -25,4 +28,15 @@ public class ServiceManager {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	public List<Post> getAllPosts () throws Exception {
+		return postRepository.findAll();
+	}
+
+	public Post inserisciPost (Post post) throws Exception {
+		try {
+			return postRepository.save(post);
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+	}
 }
